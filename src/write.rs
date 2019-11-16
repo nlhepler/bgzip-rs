fdacf8b38f6daa23bd259546ae77ae97ba7427d7
Lance Hepler <lhepler@pacb.com>
2019-11-15 22:07:22 -0800

    new compressors, 0-alloc design, address comments

--- src/write.rs
+++ src/write.rs
@@ -4,7 +4,7 @@
 //!
 //! # Example
 //! ```
-//! use bgzip::write::BGzWriter;
+//! use bgzip::write::BGzBuilder;
 //! use std::fs;
 //! use std::io;
 //! use std::io::prelude::*;
@@ -12,7 +12,7 @@
 //! # fn main() { let _ = run(); }
 //! # fn run() -> io::Result<()> {
 //! let data = b"0123456789ABCDEF";
-//! let mut writer = BGzWriter::new(fs::File::create("tmp/test2.gz")?);
+//! let mut writer = BGzBuilder::new().write(fs::File::create("tmp/test2.gz")?);
 //!
 //! for _ in 0..30000 {
 //!     writer.write(&data[..])?;
@@ -31,6 +31,35 @@ use std::io::prelude::*;
 
 const DEFAULT_BUFFER: usize = 65280;
 
+pub struct BGzBuilder {
+    lvl: Compression,
+    more_threads: usize,
+}
+
+impl BGzBuilder {
+    pub fn new() -> Self {
+        BGzBuilder {
+            lvl: Compression::default(),
+            more_threads: 0,
+        }
+    }
+
+    pub fn lvl(mut self, lvl: Compression) -> Self {
+        self.lvl = lvl;
+        self
+    }
+
+    pub fn more_threads(mut self, more_threads: usize) -> Self {
+        self.more_threads = more_threads;
+        self
+    }
+
+    pub fn write<W: io::Write + Sync + Send + 'static>(self, w: W) -> BGzWriter {
+        let BGzBuilder { lvl, more_threads } = self;
+        BGzWriter::new(w, more_threads, lvl)
+    }
+}
+
 #[derive(Debug)]
 pub struct BGzWriter {
     buffer: Vec<u8>,
@@ -38,120 +67,135 @@ pub struct BGzWriter {
     channels: Option<(
         Receiver<io::Result<Vec<u8>>>,
         Sender<io::Result<Vec<u8>>>,
+        Sender<(u64, Vec<u8>)>,
         Sender<(u64, io::Result<(usize, Vec<u8>, u32)>)>,
+        Receiver<()>,
     )>,
-    r_end: Receiver<()>,
     seq: u64,
 }
 
 impl BGzWriter {
-    pub fn new<R: io::Write + Sync + Send + 'static>(
+    fn new<R: io::Write + Sync + Send + 'static>(
         mut writer: R,
         more_threads: usize,
+        lvl: Compression,
     ) -> BGzWriter {
         let pool = ThreadPoolBuilder::new()
             .num_threads(2 + more_threads)
             .build()
             .unwrap();
         // 2 each for compressors and main, 1 for writer
-        let (s_buf, r_buf) = bounded(3 + 2 * more_threads);
+        let (s_buf, r_buf): (Sender<io::Result<Vec<u8>>>, Receiver<io::Result<Vec<u8>>>) =
+            bounded(3 + 2 * more_threads);
         // 1 for each compressor
-        let (s_out, r_out) = bounded(1 + more_threads);
-        let (s_end, r_end) = bounded(1);
-        for _ in 0..3 + 2 * more_threads {
+        let (s_enc, r_enc): (Sender<(u64, Vec<u8>)>, Receiver<(u64, Vec<u8>)>) =
+            bounded(1 + more_threads);
+        let (s_out, r_out): (
+            Sender<(u64, io::Result<(usize, Vec<u8>, u32)>)>,
+            Receiver<(u64, io::Result<(usize, Vec<u8>, u32)>)>,
+        ) = bounded(1 + more_threads);
+        let (s_end, r_end): (Sender<()>, Receiver<()>) = bounded(2 + more_threads);
+        for _ in 0..3 + more_threads {
             s_buf.send(Ok(Vec::with_capacity(DEFAULT_BUFFER))).unwrap();
         }
-        let send_buf = s_buf.clone();
-        pool.spawn(move || {
-            let mut write_block = |block: io::Result<(usize, Vec<u8>, u32)>| {
-                let (buflen, mut compressed, crc): (usize, Vec<u8>, u32) = block?;
-                let compressed_len = compressed.len() + 19 + 6;
-                let header = [
-                    0x1f,
-                    0x8b,
-                    0x08,
-                    0x04,
-                    0x00,
-                    0x00,
-                    0x00,
-                    0x00,
-                    0x02,
-                    0xff,
-                    0x06,
-                    0x00,
-                    66,
-                    67,
-                    0x02,
-                    0x00,
-                    (compressed_len & 0xff) as u8,
-                    ((compressed_len >> 8) & buflen) as u8,
-                ];
-                let wrote_bytes = writer.write(&header[..])?;
-                if wrote_bytes != header.len() {
-                    return Err(io::Error::new(io::ErrorKind::Other, "Cannot write header"));
+        for _ in 0..1 + more_threads {
+            let r_enc = r_enc.clone();
+            let s_out = s_out.clone();
+            let s_end = s_end.clone();
+            pool.spawn(move || {
+                let mut encoder =
+                    Some(DeflateEncoder::new(Vec::with_capacity(DEFAULT_BUFFER), lvl));
+                let mut compress = move |mut buffer: Vec<u8>| {
+                    let mut inner = None;
+                    std::mem::swap(&mut inner, &mut encoder);
+                    let mut crc = CrcWriter::new(inner.unwrap());
+                    crc.write(&buffer)?;
+                    let cksum = crc.crc().sum();
+                    std::mem::swap(&mut Some(crc.into_inner()), &mut encoder);
+                    let buflen = buffer.len();
+                    buffer.clear();
+                    let compressed = encoder.as_mut().unwrap().reset(buffer)?;
+                    Ok((buflen, compressed, cksum))
+                };
+                for (i, buffer) in r_enc.iter() {
+                    let compressed_result = compress(buffer);
+                    s_out.send((i, compressed_result)).unwrap();
                 }
+                s_end.send(()).unwrap();
+            });
+        }
+        {
+            let s_buf = s_buf.clone();
+            pool.spawn(move || {
+                let mut write_block = |block: io::Result<(usize, Vec<u8>, u32)>| {
+                    let (buflen, mut compressed, crc): (usize, Vec<u8>, u32) = block?;
+                    let compressed_len = compressed.len() + 19 + 6;
+                    let header = [
+                        0x1f,
+                        0x8b,
+                        0x08,
+                        0x04,
+                        0x00,
+                        0x00,
+                        0x00,
+                        0x00,
+                        0x02,
+                        0xff,
+                        0x06,
+                        0x00,
+                        66,
+                        67,
+                        0x02,
+                        0x00,
+                        (compressed_len & 0xff) as u8,
+                        ((compressed_len >> 8) & buflen) as u8,
+                    ];
+                    writer.write_all(&header[..])?;
+                    writer.write_all(&compressed[..])?;
 
-                let wrote_bytes = writer.write(&compressed[..])?;
-                if wrote_bytes != compressed.len() {
-                    return Err(io::Error::new(
-                        io::ErrorKind::Other,
-                        "Cannot write compressed data",
-                    ));
-                }
+                    let crc_bytes = super::bytes_le_u32(crc);
+                    writer.write_all(&crc_bytes[..])?;
 
-                let crc_bytes = super::bytes_le_u32(crc);
-                let wrote_bytes = writer.write(&crc_bytes[..])?;
-                if wrote_bytes != crc_bytes.len() {
-                    return Err(io::Error::new(
-                        io::ErrorKind::Other,
-                        "Cannot write CRC data",
-                    ));
-                }
+                    let buflen_bytes = super::bytes_le_u32(buflen as u32);
+                    writer.write_all(&buflen_bytes[..])?;
 
-                let buflen_bytes = super::bytes_le_u32(buflen as u32);
-                let wrote_bytes = writer.write(&buflen_bytes[..])?;
-                if wrote_bytes != buflen_bytes.len() {
-                    return Err(io::Error::new(
-                        io::ErrorKind::Other,
-                        "Cannot write CRC data",
-                    ));
-                }
-
-                compressed.clear();
-                Ok(compressed)
-            };
-            let mut seq = 0u64;
-            let mut queue = HashMap::new();
-            for (i, block) in r_out.iter() {
-                if i == seq {
-                    send_buf.send(write_block(block)).unwrap();
-                    seq += 1;
-                    while let Some(block) = queue.remove(&seq) {
-                        send_buf.send(write_block(block)).unwrap();
+                    compressed.clear();
+                    Ok(compressed)
+                };
+                let mut seq = 0u64;
+                let mut queue = HashMap::new();
+                for (i, block) in r_out.iter() {
+                    if i == seq {
+                        let buffer = write_block(block);
+                        s_buf.send(buffer).unwrap();
                         seq += 1;
+                        while let Some(block) = queue.remove(&seq) {
+                            let buffer = write_block(block);
+                            s_buf.send(buffer).unwrap();
+                            seq += 1;
+                        }
+                    } else {
+                        queue.insert(i, block);
                     }
-                } else {
-                    queue.insert(i, block);
                 }
-            }
-            let mut func = move || {
-                let eof_bytes = [
-                    0x1f, 0x8b, 0x08, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0x06, 0x00, 0x42,
-                    0x43, 0x02, 0x00, 0x1b, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                    0x00, 0x00,
-                ];
-                writer.write(&eof_bytes[..])?;
-                writer.flush()?;
-                Ok(vec![])
-            };
-            send_buf.send(func()).unwrap();
-            s_end.send(()).unwrap();
-        });
+                let mut func = move || {
+                    let eof_bytes = [
+                        0x1f, 0x8b, 0x08, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0x06, 0x00,
+                        0x42, 0x43, 0x02, 0x00, 0x1b, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00,
+                        0x00, 0x00, 0x00, 0x00,
+                    ];
+                    writer.write(&eof_bytes[..])?;
+                    writer.flush()?;
+                    Ok(vec![])
+                };
+                s_buf.send(func()).unwrap();
+                s_end.send(()).unwrap();
+            });
+        }
         BGzWriter {
             buffer: Vec::with_capacity(DEFAULT_BUFFER),
             pool,
-            channels: Some((r_buf, s_buf, s_out)),
-            r_end,
+            channels: Some((r_buf, s_buf, s_enc, s_out, r_end)),
             seq: 0,
         }
     }
@@ -167,47 +211,35 @@ impl io::Write for BGzWriter {
     }
 
     fn flush(&mut self) -> io::Result<()> {
-        let (r_buf, s_buf, s_out) = self.channels.as_mut().unwrap();
-        let mut buffer: Vec<u8> = r_buf.recv().unwrap()?;
-        let mut compressed = r_buf.recv().unwrap()?;
-        let s_buf = s_buf.clone();
-        let s_out = s_out.clone();
-        std::mem::swap(&mut self.buffer, &mut buffer);
-        let seq = self.seq;
-        self.pool.spawn(move || {
-            let func = || {
-                let crc = {
-                    let mut encoder =
-                        CrcWriter::new(DeflateEncoder::new(&mut compressed, Compression::best()));
-                    encoder.write(&buffer)?;
-                    encoder.crc().sum()
-                };
-                let buflen = buffer.len();
-                buffer.clear();
-                s_buf.send(Ok(buffer)).unwrap();
-                Ok((buflen, compressed, crc))
-            };
-            s_out.send((seq, func())).unwrap();
-        });
-        self.seq += 1;
+        if !self.buffer.is_empty() {
+            let (r_buf, _, s_enc, _, _) = self.channels.as_mut().unwrap();
+            let mut buffer: Vec<u8> = r_buf.recv().unwrap()?;
+            std::mem::swap(&mut self.buffer, &mut buffer);
+            s_enc.send((self.seq, buffer)).unwrap();
+            self.seq += 1;
+        }
         Ok(())
     }
 }
 
 impl Drop for BGzWriter {
     fn drop(&mut self) {
+        // the only way flush() fails is if the buffer channel is closed prematurely. panicking in
+        // that case is completely warranted--something has gone horribly wrong.
         self.flush().unwrap();
-        {
-            let r_buf = {
+        let r_end = {
+            let (r_buf, r_end) = {
                 let mut tmp = None;
                 std::mem::swap(&mut self.channels, &mut tmp);
-                tmp.unwrap().0
+                let (r_buf, _, _, _, r_end) = tmp.unwrap();
+                (r_buf, r_end)
             };
             // ensure we've received all the outstanding buffers
-            let _ = r_buf.iter().collect::<Vec<_>>();
-        }
+            for _ in r_buf.iter() {}
+            r_end
+        };
         // ensure the writer thread has finished
-        let _ = self.r_end.recv().unwrap();
+        for _ in r_end.iter() {}
     }
 }
 
@@ -220,8 +252,11 @@ mod test {
     #[test]
     fn test_writer() -> io::Result<()> {
         {
+            let mut writer = super::BGzBuilder::new()
+                .more_threads(3)
+                .write(fs::File::create("tmp/test.gz")?);
+
             let data = b"0123456789ABCDEF";
-            let mut writer = super::BGzWriter::new(fs::File::create("tmp/test.gz")?, 3);
             for _ in 0..30000 {
                 writer.write(&data[..])?;
             }
